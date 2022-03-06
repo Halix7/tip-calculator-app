7d3f5c16010febda143fee5ee19b27a6aa07d7d2
Rohit <halixh39@gmail.com>
2022-03-06 11:35:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+let bill = document.getElementById("bill");
+let people = document.getElementById("people1");
+let custom = document.getElementById("custom");
+let tipamount = document.getElementById("first");
+let total = document.getElementById("second");
+// let btn = document.querySelectorAll(".grid-btn *");
+let h2 = document.querySelector("[hiddenheading]");
+let tip = 0;
+let btnvalue = 15;
+// making the % tips 
+let btnfive = document.querySelectorAll(".grid-btn *");
+
+let reset = document.querySelector("[peoplezero]")
+
+function activeLink() {
+    btnfive.forEach((item) => {
+        item.classList.remove("active");
+
+    });
+    this.classList.add("active");
+    btnvalue = parseInt(document.querySelector(".active").innerHTML.replace("%", ""));
+    console.log(btnvalue);
+
+
+
+}
+btnfive.forEach((item) => {
+    item.addEventListener('click', activeLink);
+    // console.log(item.innerHTML);
+
+
+
+});
+bill.addEventListener("change", () => {
+    if (isNaN(btnvalue)) {
+        tip = ((bill.value / people.value) / 100) * parseInt(custom.value);
+    } else {
+
+        tip = ((bill.value / people.value) / 100) * btnvalue;
+    }
+    total.innerHTML = parseInt(tip) + parseInt(bill.value);
+    tipamount.innerHTML = parseInt(tip);
+
+})
+
+people.oninput = function() {
+    if (people.value == 0) {
+
+        h2.setAttribute("hiddenheading", "false");
+
+    } else {
+        reset.setAttribute("peoplezero", "false");
+        h2.setAttribute("hiddenheading", "true");
+
+        if (isNaN(btnvalue)) {
+            tip = ((bill.value / people.value) / 100) * parseInt(custom.value);
+        } else {
+
+            tip = ((bill.value / people.value) / 100) * btnvalue;
+        }
+        total.innerHTML = parseInt(tip) + parseInt(bill.value);
+        tipamount.innerHTML = parseInt(tip);
+    }
+
+
+};
+reset.addEventListener('click', () => {
+    people.value = ""
+    bill.value = ""
+
+})
